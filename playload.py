bc809242834255a5789efce79402916d5cf4c851
Swadhin Biswas <155087959+professoridx@users.noreply.github.com>
2024-01-07 17:39:14 +0600

    Update playload.py

--- playload.py
+++ playload.py
@@ -1,1 +1,34 @@
+def setplayload():
+    #select height
+    height=int(input("select height"))
+    
+    prompt=input("input your imagination")
+    width=int(input("input weight"))
+    alchemy=str(input("input true or false")).lower()
+    controlNet=""
+    controlNetType=""
+    unzoom="tiling"
+    sd_version= "v2",
+    scheduler= str(input("input true or false")).lower()
+    public= "true"
+    promptMagic= "true"
+    tiling=""
+    
+        
+    payload = {
+        "height": height,
+        "modelId": "6bef9f1b-29cb-40c7-b9df-32b51c1f67d3",
+        "prompt": prompt,
+        "width": width,
+        "alchemy": alchemy,
+        "controlNet": controlNet,
+        "controlNetType": controlNetType,
+        "unzoom": unzoom,
+        "tiling": tiling,
+        "sd_version": sd_version,
+        "scheduler": scheduler,
+        "public": public,
+        "promptMagic": promptMagic,
+    }
 
+setplayload()
